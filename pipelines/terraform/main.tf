610a44ac585b0f8e0e1f2522b22ace0c83b51a76
Felipe Antonio Teixeira <felipeantonioteixeira@BOLD-2019083.lan>
2023-01-23 15:11:49 +0000

    terraform destroy test

--- pipelines/terraform/main.tf
+++ pipelines/terraform/main.tf
@@ -20,3 +20,5 @@ resource "aws_iam_role_policy_attachment" "AmazonSSMManagedInstanceCore" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
   role       = module.ec2_instance.iam_role_name
 }
+
+
